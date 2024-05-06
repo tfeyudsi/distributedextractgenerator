1fd05b650f9143664df383a3eb03448dd4b3cab2
tfeyudsi <gs1branhill@hotmail.com>
2024-05-06 19:17:46 +0700

    update task

--- index.js
+++ index.js
@@ -1,16 +1,19 @@
-const radixSort = (arr) => {
-  const getDigit = (num, place) =>
-    Math.floor(Math.abs(num) / Math.pow(10, place)) % 10;
-  const digitCount = (num) =>
-    num === 0 ? 1 : Math.floor(Math.log10(Math.abs(num))) + 1;
-  const mostDigits = (arr) => Math.max(...arr.map((num) => digitCount(num)));
-  const maxDigits = mostDigits(arr);
-  for (let k = 0; k < maxDigits; k++) {
-    let digitBuckets = Array.from({ length: 10 }, () => []);
-    for (let i = 0; i < arr.length; i++) {
-      digitBuckets[getDigit(arr[i], k)].push(arr[i]);
-    }
-    arr = [].concat(...digitBuckets);
+function uniquePathsWithObstacles(obstacleGrid) {
+  const m = obstacleGrid.length;
+  const n = obstacleGrid[0].length;
+  const dp = new Array(m).fill(0).map(() => new Array(n).fill(0));
+  if (obstacleGrid[0][0] === 1) return 0;
+  dp[0][0] = 1;
+  for (let i = 1; i < m; i++) {
+    if (obstacleGrid[i][0] === 0) dp[i][0] = dp[i - 1][0];
   }
-  return arr;
-};
+  for (let j = 1; j < n; j++) {
+    if (obstacleGrid[0][j] === 0) dp[0][j] = dp[0][j - 1];
+  }
+  for (let i = 1; i < m; i++) {
+    for (let j = 1; j < n; j++) {
+      if (obstacleGrid[i][j] === 0) dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
+    }
+  }
+  return dp[m - 1][n - 1];
+}
