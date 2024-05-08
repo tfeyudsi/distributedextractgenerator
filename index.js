f514963b793124cca7eb04cccfe7cf2158489cff
tfeyudsi <gs1branhill@hotmail.com>
2024-05-08 08:34:06 +0700

    test task

--- index.js
+++ index.js
@@ -1,11 +1,4 @@
-function isValidBST(root) {
-  let prev = null;
-  return inorder(root);
-  function inorder(node) {
-    if (!node) return true;
-    if (!inorder(node.left)) return false;
-    if (prev !== null && node.val <= prev) return false;
-    prev = node.val;
-    return inorder(node.right);
-  }
-}
+const dropRightWhile = (arr, func) => {
+  while (arr.length > 0 && !func(arr[arr.length - 1])) arr = arr.slice(0, -1);
+  return arr;
+};
