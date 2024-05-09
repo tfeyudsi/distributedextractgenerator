3bfa1fb733a260c443f6449ba12649bf0b230062
tfeyudsi <gs1branhill@hotmail.com>
2024-05-09 19:18:41 +0700

    update sdk task commit

--- index.js
+++ index.js
@@ -1,4 +1,12 @@
-const dropRightWhile = (arr, func) => {
-  while (arr.length > 0 && !func(arr[arr.length - 1])) arr = arr.slice(0, -1);
+function bubbleSort(arr) {
+  for (let i = 0; i < arr.length - 1; i++) {
+    for (let j = 0; j < arr.length - i - 1; j++) {
+      if (arr[j] > arr[j + 1]) {
+        let temp = arr[j];
+        arr[j] = arr[j + 1];
+        arr[j + 1] = temp;
+      }
+    }
+  }
   return arr;
-};
+}
